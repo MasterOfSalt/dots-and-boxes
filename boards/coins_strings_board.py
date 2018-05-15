bc2e402651b6d14fa279de4415efe9282c1b0bc7
MasterOfSalt <lucas.bruggeman@student.kuleuven.be>
2018-05-15 22:52:51 +0200

    added some clarification for future use
    Signed-off-by: MasterOfSalt <lucas.bruggeman@student.kuleuven.be>

--- boards/coins_strings_board.py
+++ boards/coins_strings_board.py
@@ -6,6 +6,15 @@ coins_strings_board.py
 __ for the Machine Learning Project course at KU Leuven (2017-2018)
 
 """
+"""
+from:"Chapter 16: Dots-and-Boxes", Winning Ways for your Mathematical Plays, Volume 2: Games in Particular, Academic Press, pp. 507–550.
+Dots and Boxes has a dual graph form called "Strings-and-Coins".
+ This game is played on a network of coins (vertices) joined by strings (edges).
+ Players take turns cutting a string.
+ When a cut leaves a coin with no strings, the player "pockets" the coin and takes another turn.
+ The winner is the player who pockets the most coins.
+ Strings-and-Coins can be played on an arbitrary graph
+"""
 class Coins_strings_board:
     def __init__(self,nb_rows=0,nb_cols=0):
         """Create board that uses coins and strings for its represenation.
