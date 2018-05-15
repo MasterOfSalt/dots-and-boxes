09346c822647f9208193a0b63cceff1452a986da
MasterOfSalt <lucas.bruggeman@student.kuleuven.be>
2018-05-15 22:53:18 +0200

    .
    Signed-off-by: MasterOfSalt <lucas.bruggeman@student.kuleuven.be>

--- version2/alpha_beta_v2_chains.py
+++ version2/alpha_beta_v2_chains.py
@@ -1,29 +1,106 @@
+"""
+Pseudocode from https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning
+01 function alphabeta(node, depth, α, β, maximizingPlayer) is
+02     if depth = 0 or node is a terminal node then
+03         return the heuristic value of node
+04     if maximizingPlayer then
+05         v := -∞
+06         for each child of node do
+07             v := max(v, alphabeta(child, depth – 1, α, β, FALSE))
+08             α := max(α, v)
+09             if β ≤ α then
+10                 break (* β cut-off *)
+11         return v
+12     else
+13         v := +∞
+14         for each child of node do
+15             v := min(v, alphabeta(child, depth – 1, α, β, TRUE))
+16             β := min(β, v)
+17             if β ≤ α then
+18                 break (* α cut-off *)
+19         return v
+"""
 import sys
+max = sys.maxsize
+def alphabeta(board,depth,player):
+    """method description
+    :param x: _explanation
+    """
+    return maximax(board,depth,player,-max,max)
 
-def longest_chain_from(game, visited, i, j, length):
-    place = str(i) + str(j)
-    if not (i > 0 and i < game.r and j > 0 and j < game.c) or place in visited:
-        return length
-    visited[place] = True
-    # find openings
-    horizontal, vertical = game.board[i-1, j] and game.board[i+1, j], game.board[i, j-1] and game.board[i, j+1]
-    # follow each opening and return length
-    if horizontal:
-        return max(longest_chain_from(game, visited, i, j-2, length+1), longest_chain_from(game, visited, i, j+2, length+1))
-    elif vertical:
-        return max(longest_chain_from(game, visited, i-2, j, length+1), longest_chain_from(game, visited, i+2, j, length+1))
-    return 0
+def done(move):
+    """method description
+    :param x: _explanation
+    """
+    return move[0],move[1],1337
 
-def find_longest_chain(game):
-    longest_chain = 0
-    for i in range(1,game.r,2):
-        for j in range(1,game.c,2):
-            longest_chain = max(longest_chain, longest_chain_from(game, dict(), i, j, 0))
-    return longest_chain
+def maximax(board, depth, player,alpha,beta,move=(0,0)):
+    """method description
+    :param x: _explanation
+    """
+    if depth == 0:
+        return done(move)
+    score = board.boxes[player-1]
+    for x, y in board.free_lines():
+        current = board.copy()
+        if current.fill_line(x, y, player):
+            (a,b,current_score) = maximax(current,depth-1,player,alpha,beta,(x,y))
+        else:
+            (a,b,current_score) = minimin(current,depth-1,player,alpha,beta,(x,y))
+        if current_score > score:
+            move = (x,y)
+            score = current_score
+        alpha = bigger(score, alpha)
+        #print (score)
+        if beta <= alpha:
+            break
+    return (move[0],move[1],score)
 
-def get_chain_count(game):
-    chain_count = 0
-    for i in range(1,game.r,2):
-            if longest_chain_from(game, dict(), i, j, 0) >= 3:
-                chain_count += 1
-    return chain_count
+def minimin(board, depth,player,alpha,beta,move=(0,0)):
+    """method description
+    :param x: _explanation
+    """
+    if depth == 0:
+        return done(move)
+    score = board.max_points()
+    for x, y in board.free_lines():
+        current = board.copy()
+        if current.fill_line(x, y, player):
+            (a,b,current_score) = minimin(current,depth-1,player,alpha,beta,(x,y))
+        else:
+            (a,b,current_score) = maximax(current,depth-1,player,alpha,beta,(x,y))
+        if current_score < score:
+            move = (x,y)
+            score = current_score
+        beta = smaller(beta, score)
+        if beta <= alpha:
+            break
+        #print (score)
+    return (move[0],move[1],score)
+
+def bigger(a,b):
+    """method description
+    :param x: _explanation
+    """
+    if a > b:
+        return a
+    else:
+        return b
+
+def smaller(a,b):
+    """method description
+    :param x: _explanation
+    """
+    if a < b:
+        return a
+    else:
+        return b
+
+def swap_player(player):
+    """method description
+    :param x: _explanation
+    """
+    if player == 1:
+        return 2
+    else:
+        return 1
