533ea79ee6657be85a8e3a8c5aaa4ce8b19278b9
MasterOfSalt <lucas.bruggeman@student.kuleuven.be>
2018-07-06 13:54:41 +0200

    added file support for mcts
    Signed-off-by: MasterOfSalt <lucas.bruggeman@student.kuleuven.be>

--- boards/mcst.py
+++ boards/mcst.py
@@ -9,3 +9,24 @@ import glob
 import errno
 
 class MonteCarloSearchTree:
+    def __init__(self,n,m):
+        self.dimensions = 'version5/'+str(n) + 'x' +str(m)
+        self.tree = {}
+        if os.path.isfile(self.dimensions+'/tree.data'):
+            file = open(self.dimensions+'/tree.data')
+            self.tree = json.loads(file.read())
+            file.close()
+        else:
+            self.tree['nodes'] = []
+            init = json.dumps(self.tree)
+            os.makedirs(self.dimensions)
+            os.makedirs(self.dimensions+'/games')
+            os.makedirs(self.dimensions+'/games/processed')
+            os.makedirs(self.dimensions+'/games/unprocessed')
+            file = open(self.dimensions+'/tree.data', 'a+')
+            file.write(init)
+            file.close()
+
+
+
+# add_game(['1,2,h','1,1,v','1,3,h','1,3,h','1,3,h','1,3,h','1,3,h','1,3,h','1,3,h','1,3,h','1,3,h','1,3,h','1,3,h','1,3,h','1,3,h','1,3,h','1,3,h'],1)
