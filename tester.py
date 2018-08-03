9b73cd90576669afb903a231f6a5cd547ce62c94
MasterOfSalt <lucas.bruggeman@student.kuleuven.be>
2018-08-03 14:54:29 +0200

    added test cases for 90 degrees transformation
    Signed-off-by: MasterOfSalt <lucas.bruggeman@student.kuleuven.be>

--- tester.py
+++ tester.py
@@ -4,3 +4,20 @@ def transform_90(coord,n):
         return y,(n-x),"v"
     if o == "v":
         return y,(n-x)-1,"h"
+
+
+print(transform_90((0,0,"h"),6))
+print(transform_90((0,1,"h"),6))
+print(transform_90((0,2,"h"),6))
+
+print(transform_90((1,0,"h"),6))
+print(transform_90((1,1,"h"),6))
+print(transform_90((1,2,"h"),6))
+
+print(transform_90((0,0,"v"),6))
+print(transform_90((1,0,"v"),6))
+print(transform_90((2,0,"v"),6))
+
+print(transform_90((0,1,"v"),6))
+print(transform_90((1,1,"v"),6))
+print(transform_90((2,1,"v"),6))
