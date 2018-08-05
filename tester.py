027a35f3d8c74b586188e4b7712557bc6585292b
MasterOfSalt <lucas.bruggeman@student.kuleuven.be>
2018-08-05 17:22:16 +0200

    altered code for transformations
    Signed-off-by: MasterOfSalt <lucas.bruggeman@student.kuleuven.be>

--- tester.py
+++ tester.py
@@ -1,32 +1,30 @@
-
-
 def transform_90_nxn(coord,n):
     x,y,o = coord
     if o == "h":
-        return y,(n-x),"v"
+        return y,abs(n-x),"v"
     if o == "v":
-        return y,(n-x)-1,"h"
+        return y,abs(n-x-1),"h"
 
 def transform_horizontal_nxn(coord,n):
     x,y,o = coord
     if o == "h":
-        return (n-x),y,o
+        return abs(n-x),y,o
     if o == "v":
-        return (n-x)-1,y,o
+        return abs(n-x-1),y,o
 
 def transform_vertical_nxn(coord,n):
     x,y,o = coord
     if o == "h":
-        return x,(n-y-1),o
+        return x,abs(n-y-1),o
     if o == "v":
-        return x,(n-x),o
+        return x,abs(n-x),o
+
+
+
 def transform_180_nxn(coord,n):
     return transform_90_nxn(transform_90_nxn(coord,n),n)
 def transform_270_nxn(coord,n):
     return transform_90_nxn(transform_180_nxn(coord,n),n)
-print(transform_90_nxn((0,0,"h"),6))
-print(transform_180_nxn((0,0,"h"),6))
-print(transform_270_nxn((0,0,"h"),6))
 
 # print(transform_90((0,2,"h"),6))
 #
